cadd98ce6e4929c086aff1ac61e42d5afabbb79b
abhishek <abhishek.satapathy01@gmail.com>
2020-04-16 13:09:43 +0530

    Alternate solution to  189. Rotate Array  of LeetCode in CPP

--- LeetCode/189-rotate-array/rotate.cpp
+++ LeetCode/189-rotate-array/rotate.cpp
@@ -1,11 +1,28 @@
+// class Solution {
+// public:
+//     void rotate(vector<int>& nums, int k) {
+//         if(nums.size()==1 || nums.size()==0)
+//             return;
+//         int n = nums.size();
+//         if(k>n)
+//             k=k%n;
+//         std::rotate(nums.begin(), nums.begin() + nums.size() - k, nums.end());
+//     }
+// };
+
+
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
         if(nums.size()==1 || nums.size()==0)
             return;
         int n = nums.size();
-        if(k>n)
+         if(k>n)
             k=k%n;
-        std::rotate(nums.begin(), nums.begin() + nums.size() - k, nums.end());
+        reverse(nums.begin(),nums.end());
+        reverse(nums.begin(),nums.begin()+k);
+        reverse(nums.begin()+k,nums.end());
+       
+        // std::rotate(nums.begin(), nums.begin() + nums.size() - k, nums.end());
     }
 };
